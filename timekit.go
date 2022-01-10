dceb5d604f8085913e788b63783c46094d462b29
bartmika <bart@mikasoftware.com>
2022-01-09 23:23:45 -0500

    TYPOFIX

--- timekit.go
+++ timekit.go
@@ -34,7 +34,7 @@ func Midnight(now func() time.Time) time.Time {
 	return time.Date(dt.Year(), dt.Month(), dt.Day(), 0, 0, 0, 0, dt.Location())
 }
 
-// Midnight will return tomorrows date at 12 o’clock (or 0:00) during the night.
+// MidnightTomorrow will return tomorrows date at 12 o’clock (or 0:00) during the night.
 func MidnightTomorrow(now func() time.Time) time.Time {
 	dt := now()
 	return time.Date(dt.Year(), dt.Month(), dt.Day()+1, 0, 0, 0, 0, dt.Location())
