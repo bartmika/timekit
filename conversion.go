abdeeac8bd400eb7f1197a82681fccad48958967
bartmika <bart@mikasoftware.com>
2022-01-24 23:16:41 -0500

    Refactored function name.

--- conversion.go
+++ conversion.go
@@ -4,7 +4,9 @@ import (
 	"time"
 )
 
-// TimeFromJavaScriptGetTime will convert the number of milliseconds since the Unix Epoch parameter into Golang `time` format. As a result, the output of the JavaScript `getTime()` function can be used as the parameter in this function.
-func TimeFromJavaScriptGetTime(i int64) time.Time {
+// ParseJavaScriptTime will convert the number of milliseconds since the Unix Epoch parameter into Golang `time` format. As a result, the output of the JavaScript `getTime()` function can be used as the parameter in this function.
+func ParseJavaScriptTime(i int64) time.Time {
+	// Special thanks JavaScript timestamp to golang time.Time
+	// https://gist.github.com/alextanhongpin/3b6b2ee47665ac9c1c32c805b86380a6
 	return time.Unix(i/1000, (i%1000)*1000*1000)
 }
--- conversion_test.go
+++ conversion_test.go
@@ -5,14 +5,14 @@ import (
 	"time"
 )
 
-func TestTimeFromJavaScriptGetTime(t *testing.T) {
+func TestParseJavaScriptTime(t *testing.T) {
 	// EXAMPLE JAVASCRIPT CODE
 	// var dt = new Date()
 	// console.log(dt.getTime()) // 1643082322380
 	// console.log(dt) // Mon Jan 24 2022 22:45:22 GMT-0500 (Eastern Standard Time)
 
-	sample := int64(1643082322380)
-	actual := TimeFromJavaScriptGetTime(sample)
+	jsTime := int64(1643082322380)
+	actual := ParseJavaScriptTime(jsTime)
 	loc, _ := time.LoadLocation("America/Toronto")
 	expected := time.Date(2022, 1, 24, 22, 45, 22, 380000000, loc) // 2022-01-24 22:45:22.38 -0500 EST
 	if actual.Equal(expected) == false {
