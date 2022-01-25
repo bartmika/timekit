96d4ef18d96a7a2a6264fe2deb24d16eadf17a75
bartmika <bart@mikasoftware.com>
2022-01-24 23:59:09 -0500

    Added another useful func.

--- conversion.go
+++ conversion.go
@@ -1,6 +1,7 @@
 package timekit
 
 import (
+	"strconv"
 	"time"
 )
 
@@ -9,4 +10,16 @@ func ParseJavaScriptTime(i int64) time.Time {
 	// Special thanks JavaScript timestamp to golang time.Time
 	// https://gist.github.com/alextanhongpin/3b6b2ee47665ac9c1c32c805b86380a6
 	return time.Unix(i/1000, (i%1000)*1000*1000)
+}
+
+// ParseJavaScriptTimeString will convert the string of milliseconds integers since the Unix Epoch parameter into Golang `time` format. As a result, the output of the JavaScript `getTime()` function can be used as the parameter in this function.
+func ParseJavaScriptTimeString(s string) (time.Time, error) {
+	i, valErr := strconv.ParseInt(s, 10, 64)
+	if valErr != nil {
+		return time.Now(), valErr
+	}
+
+	// Special thanks JavaScript timestamp to golang time.Time
+	// https://gist.github.com/alextanhongpin/3b6b2ee47665ac9c1c32c805b86380a6
+	return time.Unix(i/1000, (i%1000)*1000*1000), nil
 }
--- conversion_test.go
+++ conversion_test.go
@@ -19,3 +19,31 @@ func TestParseJavaScriptTime(t *testing.T) {
 		t.Errorf("Incorrect date, got %s but was expecting %s", actual, expected)
 	}
 }
+
+func TestParseJavaScriptTimeString(t *testing.T) {
+	// EXAMPLE JAVASCRIPT CODE
+	// var dt = new Date()
+	// console.log(dt.getTime()) // 1643082322380
+	// console.log(dt) // Mon Jan 24 2022 22:45:22 GMT-0500 (Eastern Standard Time)
+
+	jsTimeStr := "1643082322380"
+	actual, err := ParseJavaScriptTimeString(jsTimeStr)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Case 1 of 2 - Correct string.
+
+	loc, _ := time.LoadLocation("America/Toronto")
+	expected := time.Date(2022, 1, 24, 22, 45, 22, 380000000, loc) // 2022-01-24 22:45:22.38 -0500 EST
+	if actual.Equal(expected) == false {
+		t.Errorf("Incorrect date, got %s but was expecting %s", actual, expected)
+	}
+
+	// Case 2 of 2 - Incorrect string.
+
+	_, err = ParseJavaScriptTimeString("-------")
+	if err == nil {
+		t.Error("Incorrect error, should be not be nil but is nil!")
+	}
+}
